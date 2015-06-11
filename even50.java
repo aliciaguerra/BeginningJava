9b5a41a91f84a747db2617a826323d27171b6c76
Alicia Guerra <aiguerra@ucsd.edu>
2015-06-11 00:42:13 -0700

    Create even50.java

---
+++ even50.java
@@ -0,0 +1,6 @@
+if(number%2 == 0)  //Test if it is even
+if(number<50)      //Output a message if number is <50
+System.out.println("You have got an even number <50," + number);
+
+else
+System.out.println)"You have got an odd number," + number); //It is odd
