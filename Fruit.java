d492f11f8b2fc1dbd306272fba7c70567907ad0c
Alicia Guerra <aiguerra@ucsd.edu>
2015-06-10 19:21:01 -0700

    Create Fruit.java

---
+++ Fruit.java
@@ -0,0 +1,14 @@
+public class Fruit
+{
+public static void main(String[] args) {
+//Declare and initialize three variables.
+int oranges=5;
+int apples=10;
+int fruit=0;
+
+fruit=oranges+apples; //Calculate the total fruit
+System.out.println("A totally fruity program");
+//Display the result.
+System.out.println("Total fruit " + fruit);
+}
+}
